250f8bfcda7064df32bd267980d09bcd0f36933c
Sachin <sachinrao2298@gmail.com>
2020-12-09 13:16:12 +0530

    prot

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyparser = require("body-parser");
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -52,5 +52,5 @@ app.get("/about",function(req,res){
 
 
 app.listen(port, function(){
-    console.log("Server is Running on port 3000");
+    console.log("Server is Running on port " + port);
 });
